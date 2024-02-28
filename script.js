1955af7fbe0b3bc0a75f7c4a793d26a7320f2e2c
Salemelmabrouk <149633706+Salemelmabrouk@users.noreply.github.com>
2024-02-28 12:40:25 +0100

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function activateSearch() {
+  var searchDiv = document.querySelector('.search');
+  searchDiv.classList.add('active');
+  var inputField = searchDiv.querySelector('.input');
+  inputField.focus();
+}
